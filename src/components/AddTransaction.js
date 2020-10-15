6ff10a3778a3b3718b2ddde16a6526bb21cc7228
Trex1888 <harpua123@gmail.com>
2020-10-15 02:27:38 -0500

    3rd commit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,7 +4,6 @@ import { GlobalContext } from "../context/GlobalState";
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
-
   const { addTransaction } = useContext(GlobalContext);
 
   const onSubmit = (e) => {
@@ -21,7 +20,7 @@ export const AddTransaction = () => {
 
   return (
     <>
-      <h3>Add new transaction</h3>
+      <h3>Add New Transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
@@ -29,22 +28,22 @@ export const AddTransaction = () => {
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
+            placeholder="Enter Text..."
           />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
-            (negative - expense, positive - income)
+            ($ Negative - Expense | $ Positive - Income)
           </label>
           <input
             type="number"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
-            placeholder="Enter amount..."
+            placeholder="Enter Amount..."
           />
         </div>
-        <button className="btn">Add transaction</button>
+        <button className="btn">Add Transaction</button>
       </form>
     </>
   );
